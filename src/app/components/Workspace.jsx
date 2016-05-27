cdfa36bd0beda10bf2ee1d7a8e64d291c94b7ad2
Tharun M Paul <tharun@nineleaps.com>
2016-05-28 02:06:37 +0530

    Restore old workspace

--- src/app/components/Workspace.jsx
+++ src/app/components/Workspace.jsx
@@ -3,58 +3,18 @@ import SceneryPage from "scenery/SceneryPage";
 import ProductPage from "product-card/ProductPage";
 import DataStore from "../DataStore";
 
-import {
-  Appear, BlockQuote, Cite, CodePane, Code, Deck, Fill, Fit,
-  Heading, Image, Layout, ListItem, List, Quote, Spectacle, Slide, Text
-} from "spectacle";
-require("normalize.css");
-require("spectacle/lib/themes/default/index.css");
-
-import createTheme from "spectacle/lib/themes/default";
-
-const theme = createTheme({
-  primary: "#dddddd"
-});
 
 export default class Workspace extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      activeIndex: 0,
-      childrenLength: 3
-    };
+    this.state = {};
   }
 
   render() {
-    let styles = this.getStyle();
-    let children = [ <SceneryPage />, <ProductPage /> ];
     return (
-      <Spectacle theme={theme}>
-        <Deck transitionDuration={500} >
-            {children.length > 0 && children.map((item, index) => {
-              return (
-                <Slide key={index} transition={[ "slide" ]} bgColor="primary" className="stupid">
-                    {item}
-                </Slide>);
-            })}
-        </Deck>
-      </Spectacle>
+      <div>
+        <ProductPage/>
+      </div>
     );
-  }
-
-  assignIndentation(index, componentStyle) {
-    if(index === this.state.activeIndex) {
-      componentStyle["left"] = 0;
-    } else {
-      componentStyle["left"] = index * screen.width;
-    }
-  }
-
-  getStyle() {
-    return {
-      component: {
-        position: "absolute"
-      }
-    };
   }
 }
