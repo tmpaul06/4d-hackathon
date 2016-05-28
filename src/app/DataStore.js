d92711cf229ab818dd25977a1d4dd3523cc5f2e6
Tharun M Paul <tharun@nineleaps.com>
2016-05-28 13:49:41 +0530

    Add another data stack

--- src/app/DataStore.js
+++ src/app/DataStore.js
@@ -78,14 +78,32 @@ class DataStore {
       }, 
       {
         x: 1,
-        y: 0 
+        y: 2 
       },
       {
         x: 2,
-        y: 0 
+        y: 1 
       }, {
         x: 3,
         y: 0
+      } ],
+      stats: [ {
+        x: 2,
+        y: 0 
+      }, 
+      {
+        x: 0,
+        y: 10 
+      },
+      {
+        x: 3,
+        y: 3
+      }, {
+        x: 100,
+        y: 2
+      }, {
+        x: 6,
+        y: 2
       } ]
     };
     this.callback = function() {};
