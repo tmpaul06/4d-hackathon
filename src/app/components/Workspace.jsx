70c4be2e51600de464d43aed12d7e05c63a14f7a
Amit-Nineleaps <amit@nineleaps.com>
2016-05-28 13:43:49 +0530

    Presentation

--- src/app/components/Workspace.jsx
+++ src/app/components/Workspace.jsx
@@ -5,15 +5,16 @@ import DataStore from "../DataStore";
 
 import {
   Appear, BlockQuote, Cite, CodePane, Code, Deck, Fill, Fit,
-  Heading, Image, Layout, ListItem, List, Quote, Spectacle, Slide, Text
+  Heading, Image, Layout, ListItem, List,Link, Quote, Spectacle, Slide, Text
 } from "spectacle";
 require("normalize.css");
 require("spectacle/lib/themes/default/index.css");
-
+import preloader from "spectacle/lib/utils/preloader";
 import createTheme from "spectacle/lib/themes/default";
 
 const theme = createTheme({
-  primary: "#dddddd"
+  primary: "#ff4081",
+  display: "block"
 });
 
 export default class Workspace extends React.Component {
@@ -31,15 +32,83 @@ export default class Workspace extends React.Component {
     return (
       <Spectacle theme={theme}>
         <Deck transitionDuration={500} >
-            {children.length > 0 && children.map((item, index) => {
-              return (
-                <Slide key={index} transition={[ "slide" ]} bgColor="primary" className="stupid">
-                    {item}
-                </Slide>);
-            })}
+          <Slide transition={[ "zoom" ]} bgColor="primary">
+            <Heading size={1} caps lineHeight={2} textColor="black">
+              4D
+            </Heading>
+            <Heading size={2} fit caps textColor="white">
+              Data Driven Dynamic Design
+            </Heading>
+          </Slide>
+          <Slide transition={[ "fade" ]} bgColor="secondary" textColor="primary" >
+            <Heading size={1} caps textColor="white">
+              What ?
+            </Heading>
+            <List>
+              <Appear><ListItem>Design Component : Look & Behaviour</ListItem></Appear>
+            </List>
+            <List>
+              <Appear><ListItem>Data Driven</ListItem></Appear>
+            </List>
+            <List>
+              <Appear><ListItem>Reusable and Composable</ListItem></Appear>
+            </List>
+          </Slide>
+          <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
+            <Heading size={1} caps textColor="white">
+              Why ?
+            </Heading>
+            <List>
+              <Appear><ListItem>Expose</ListItem></Appear>
+            </List>
+            <List>
+              <Appear><ListItem>Experiment</ListItem></Appear>
+            </List>
+          </Slide>
+          <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
+            <Heading size={1} caps textColor="white">
+              Expose
+            </Heading>
+            <List>
+              <Appear><ListItem>Point 1</ListItem></Appear>
+            </List>
+            <List>
+              <Appear><ListItem>Point 2</ListItem></Appear>
+            </List>
+            <List>
+              <Appear><ListItem>Point 3</ListItem></Appear>
+            </List>
+          </Slide>
+          <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
+            <Heading size={1} caps textColor="white">
+              Explore
+            </Heading>
+            <List>
+              <Appear><ListItem>Point 1</ListItem></Appear>
+            </List>
+            <List>
+              <Appear><ListItem>Point 2</ListItem></Appear>
+            </List>
+            <List>
+              <Appear><ListItem>Point 3</ListItem></Appear>
+            </List>
+          </Slide>
+          <Slide transition={[ "fade" ]} bgColor="secondary" textColor="primary">
+            <Heading size={1} caps textColor="white">
+              Experiment
+            </Heading>
+            <List>
+              <Appear><ListItem>Point 1</ListItem></Appear>
+            </List>
+            <List>
+              <Appear><ListItem>Point 2</ListItem></Appear>
+            </List>
+            <List>
+              <Appear><ListItem>Point 3</ListItem></Appear>
+            </List>
+          </Slide>
         </Deck>
-      </Spectacle>
-    );
+      </Spectacle>);
   }
 
   assignIndentation(index, componentStyle) {
